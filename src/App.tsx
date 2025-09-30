06ce593959ceb33f3a368846c694113764d4185b
Michael Mescua <mescua@outlook.com>
2025-09-30 06:35:30 -0500

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+   <main>
+      <h1 className='text-rose-600'>Hello!</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': `${path.resolve(__dirname, './src/assets')}`,
+			'@components': `${path.resolve(__dirname, './src/components')}`,
+			'@context': `${path.resolve(__dirname, './src/context')}`,
+			'@hooks': `${path.resolve(__dirname, './src/hooks')}`,
+			'@routes': `${path.resolve(__dirname, './src/routes')}`,
+			'@services': `${path.resolve(__dirname, './src/services')}`,
+			'@styles': `${path.resolve(__dirname, './src/styles')}`,
+			'@utils': `${path.resolve(__dirname, './src/utils')}`,
+		},
+	},
+})
